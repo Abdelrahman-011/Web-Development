43dace5ee21bcc964805ca014626fe04e7c97fed
Abdelrahman-011 <145559733+Abdelrahman-011@users.noreply.github.com>
2024-12-13 00:46:20 +0200

    Fixed package.json and dependencies

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const db = require('./Config/dbConfig'); // Database connection
-const authRoutes = require('./routes/authRoutes'); // Authentication routes
-const cartRoutes = require('./routes/cartRoutes'); // Cart routes
+const authRoutes = require('./Routes/authRoutes'); // Authentication routes
+const cartRoutes = require('./Routes/cartRoutes'); // Cart routes
 
 // Initialize Express app
 const app = express();
@@ -29,7 +29,7 @@ app.use('/api/auth', authRoutes); // Authentication routes
 app.use('/api/cart', cartRoutes); // Cart routes
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
